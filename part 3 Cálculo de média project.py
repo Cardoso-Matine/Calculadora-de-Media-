a8bb0202ea66d0c8bb8122b58283d04692b9a57b
Cardoso <cardoso.matine27@example.com>
2024-08-11 14:31:08 -0700

    novo paragrafos

--- part 3 Cálculo de média project.py
+++ part 3 Cálculo de média project.py
@@ -9,6 +9,8 @@ media= 0.0
 
 n1= float(input("digite a prmeira nota: "))
 n2= float(input("ditite a segunda notha: "))
+n2= float(input("ditite a segunda notha: "))
+n2= float(input("ditite a segunda notha: "))
 
 
 #calculo
