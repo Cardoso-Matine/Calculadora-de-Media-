5e78d678a86203b99671a4a04afe435265cc9804
Cardoso <cardoso.matine27@example.com>
2024-08-11 14:47:03 -0700

    volta ao normal v2

--- part 3 Cálculo de média project.py
+++ part 3 Cálculo de média project.py
@@ -8,7 +8,7 @@ n1=n2=0.0 # duas variaveis sem nota
 media= 0.0
 
 n1= float(input("digite a prmeira nota: "))
-n2= float(input("ditite a segunda notha: "))
+n2= float(input("digite a segunda nota: "))
 
 
 #calculo
